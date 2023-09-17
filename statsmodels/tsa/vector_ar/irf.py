96c5eaf10cddab5f2e8ad236ebd749e2d443ac3b
TianzhuQin <Maple_Optboy@outlook.com>
2023-09-17 13:57:54 +0800

    update plot axes

--- statsmodels/tsa/vector_ar/irf.py
+++ statsmodels/tsa/vector_ar/irf.py
@@ -236,6 +236,70 @@ class BaseIRAnalysis:
                                      stderr_type=stderr_type)
         return fig, axes
 
+    def plot_cum_effects_axes(self, orth=False, *, impulse=None, response=None,
+                         signif=0.05, plot_params=None, figsize=(10, 10),
+                         subplot_params=None, plot_stderr=True,
+                         stderr_type='asym', repl=1000, seed=None, axes=None):
+        """
+        Plot cumulative impulse response functions
+
+        Parameters
+        ----------
+        orth : bool, default False
+            Compute orthogonalized impulse responses
+        impulse : {str, int}
+            variable providing the impulse
+        response : {str, int}
+            variable affected by the impulse
+        signif : float (0 < signif < 1)
+            Significance level for error bars, defaults to 95% CI
+        subplot_params : dict
+            To pass to subplot plotting funcions. Example: if fonts are too big,
+            pass {'fontsize' : 8} or some number to your taste.
+        plot_params : dict
+
+        figsize: (float, float), default (10, 10)
+            Figure size (width, height in inches)
+        plot_stderr : bool, default True
+            Plot standard impulse response error bands
+        stderr_type : str
+            'asym': default, computes asymptotic standard errors
+            'mc': monte carlo standard errors (use rpl)
+        repl : int, default 1000
+            Number of replications for monte carlo standard errors
+        seed : int
+            np.random.seed for Monte Carlo replications
+        """
+
+        if orth:
+            title = 'Cumulative responses responses (orthogonalized)'
+            cum_effects = self.orth_cum_effects
+            lr_effects = self.orth_lr_effects
+        else:
+            title = 'Cumulative responses'
+            cum_effects = self.cum_effects
+            lr_effects = self.lr_effects
+
+        if stderr_type not in ['asym', 'mc']:
+            raise ValueError("`stderr_type` must be one of 'asym', 'mc'")
+        else:
+            if stderr_type == 'asym':
+                stderr = self.cum_effect_cov(orth=orth)
+            if stderr_type == 'mc':
+                stderr = self.cum_errband_mc(orth=orth, repl=repl,
+                                             signif=signif, seed=seed)
+        if not plot_stderr:
+            stderr = None
+
+        fig, axes = plotting.irf_grid_plot_axes(cum_effects, stderr, impulse, response,
+                                     self.model.names, title, signif=signif,
+                                     hlines=lr_effects,
+                                     subplot_params=subplot_params,
+                                     plot_params=plot_params,
+                                     figsize=figsize,
+                                     stderr_type=stderr_type,
+                                     axes=axes)
+        return fig, axes
 
 class IRAnalysis(BaseIRAnalysis):
     """
--- statsmodels/tsa/vector_ar/plotting.py
+++ statsmodels/tsa/vector_ar/plotting.py
@@ -245,6 +245,102 @@ def irf_grid_plot(values, stderr, impcol, rescol, names, title,
 
     return fig, axes
 
+def irf_grid_plot_axes(values, stderr, impcol, rescol, names, title,
+                  signif=0.05, hlines=None, subplot_params=None,
+                  plot_params=None, figsize=(10,10), stderr_type='asym', axes=None):
+    """
+    Reusable function to make flexible grid plots of impulse responses and
+    comulative effects
+
+    values : (T + 1) x k x k
+    stderr : T x k x k
+    hlines : k x k
+    """
+    import matplotlib.pyplot as plt
+    
+    nrows, ncols, to_plot = _get_irf_plot_config(names, impcol, rescol)
+
+    if axes is None:
+        if subplot_params is None:
+            subplot_params = {}
+        if plot_params is None:
+            plot_params = {}
+
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols, sharex=True,
+                                squeeze=False, figsize=figsize)
+
+        # fill out space
+        adjust_subplots()
+
+        fig.suptitle(title, fontsize=14)
+
+        subtitle_temp = r'%s$\rightarrow$%s'
+
+        k = len(names)
+
+        rng = lrange(len(values))
+        for (j, i, ai, aj) in to_plot:
+            ax = axes[ai][aj]
+
+            # HACK?
+            if stderr is not None:
+                if stderr_type == 'asym':
+                    sig = np.sqrt(stderr[:, j * k + i, j * k + i])
+                    plot_with_error(values[:, i, j], sig, x=rng, axes=ax,
+                                    alpha=signif, value_fmt='b', stderr_type=stderr_type)
+                if stderr_type in ('mc','sz1','sz2','sz3'):
+                    errs = stderr[0][:, i, j], stderr[1][:, i, j]
+                    plot_with_error(values[:, i, j], errs, x=rng, axes=ax,
+                                    alpha=signif, value_fmt='b', stderr_type=stderr_type)
+            else:
+                plot_with_error(values[:, i, j], None, x=rng, axes=ax,
+                                value_fmt='b')
+
+            ax.axhline(0, color='k')
+
+            if hlines is not None:
+                ax.axhline(hlines[i,j], color='k')
+
+            sz = subplot_params.get('fontsize', 12)
+            ax.set_title(subtitle_temp % (names[j], names[i]), fontsize=sz)
+    
+    else:
+        adjust_subplots()
+
+        fig.suptitle(title, fontsize=14)
+
+        subtitle_temp = r'%s$\rightarrow$%s'
+
+        k = len(names)
+
+        rng = lrange(len(values))
+        for (j, i, ai, aj) in to_plot:
+            ax = axes[ai][aj]
+
+            # HACK?
+            if stderr is not None:
+                if stderr_type == 'asym':
+                    sig = np.sqrt(stderr[:, j * k + i, j * k + i])
+                    plot_with_error(values[:, i, j], sig, x=rng, axes=ax,
+                                    alpha=signif, value_fmt='b', stderr_type=stderr_type)
+                if stderr_type in ('mc','sz1','sz2','sz3'):
+                    errs = stderr[0][:, i, j], stderr[1][:, i, j]
+                    plot_with_error(values[:, i, j], errs, x=rng, axes=ax,
+                                    alpha=signif, value_fmt='b', stderr_type=stderr_type)
+            else:
+                plot_with_error(values[:, i, j], None, x=rng, axes=ax,
+                                value_fmt='b')
+
+            ax.axhline(0, color='k')
+
+            if hlines is not None:
+                ax.axhline(hlines[i,j], color='k')
+
+            sz = subplot_params.get('fontsize', 12)
+            ax.set_title(subtitle_temp % (names[j], names[i]), fontsize=sz)
+
+    return fig, axes
+
 
 def _get_irf_plot_config(names, impcol, rescol):
     nrows = ncols = k = len(names)
